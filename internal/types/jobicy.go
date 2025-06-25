7ddec7a2043a9bc8235ef42f42627390c1e53ac4
David Mebo <krankd@proton.me>
2025-06-25 11:34:05 +0100

    removed commented code since they are no longer needed, comments on what to add in later date

--- internal/types/jobicy.go
+++ internal/types/jobicy.go
@@ -6,11 +6,13 @@ type Jobicy struct {
 }
 
 // JobicyResponse represents the API response structure
+// should be data absolutely necessary to display!
+// sometimes the structure has its data contained in an array
 type JobicyResponse struct {
 	Jobs []Job `json:"jobs"`
 }
 
-// Job represents a single job posting
+// Job represents a single job posting out of the response
 type Job struct {
 	ID          int    `json:"id"`
 	URL         string `json:"url"`
@@ -18,9 +20,12 @@ type Job struct {
 	CompanyName string `json:"companyName"`
 	JobGeo      string `json:"jobGeo"`
 	//JobType     string `json:"jobType"`	// supposed to be an array of type of jobs
+	// add job industry
 	JobLevel   string `json:"jobLevel"`
-	JobExcerpt string `json:"jobExcerpt"`
+	JobExcerpt string `json:"jobExcerpt"` // replace with jobdescription and truncate excessively long ones?
 	PubDate    string `json:"pubDate"`
 	Salary_min string `json:"salary_min"`
 	Salary_max string `json:"salary_max"`
+	// salary currency
+	// salaryperiod
 }
